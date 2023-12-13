88586bc0e030621098e30faca41e516792dbd519
Tom Cumming <mail@tomcumming.co.uk>
2023-12-13 20:33:16 +0000

    Add hls 25 and ghc 98

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Haskell binaries";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
   outputs = { self, nixpkgs }: {
@@ -20,6 +20,30 @@
               sha256 = "4c30WJJrLq9S0qgofZmpZQQP+QURcfXDt0ZwSc8OshM=";
             };
             sourceRoot = "ghc-9.6.3-aarch64-apple-darwin";
+            dontPatch = true;
+            dontBuild = true;
+            dontStrip = true;
+            configurePhase = ''
+              ./configure
+              make lib/settings
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r bin $out/bin
+              cp -r lib $out/lib
+            '';
+          };
+        ghc98 =
+          let
+            url = https://downloads.haskell.org/~ghc/9.8.1/ghc-9.8.1-aarch64-apple-darwin.tar.xz;
+          in
+          pkgs.stdenv.mkDerivation {
+            name = "ghc98";
+            src = pkgs.fetchurl {
+              url = url;
+              sha256 = "o4uwb2DJLTSOPuknpKiOiEiPh06Z9EDQYkSk5Pnbnjs=";
+            };
+            sourceRoot = "ghc-9.8.1-aarch64-apple-darwin";
             dontPatch = true;
             dontBuild = true;
             dontStrip = true;
@@ -56,6 +80,32 @@
               for f in ${ghc96}/lib/aarch64-osx-ghc-9.6.3/*.dylib
               do
                 ln -s $f $out/lib/9.6.3/
+              done
+            '';
+          };
+        hls25ghc98 =
+          let
+            url = https://downloads.haskell.org/~hls/haskell-language-server-2.5.0.0/haskell-language-server-2.5.0.0-aarch64-apple-darwin.tar.xz;
+          in
+          pkgs.stdenv.mkDerivation {
+            name = "hls25ghc98";
+            src = pkgs.fetchurl {
+              url = url;
+              sha256 = "LlCD6/f8ndPFqjEFn5M2vsRAf/+yG5OiDey0npz4gKQ=";
+            };
+            sourceRoot = "haskell-language-server-2.5.0.0";
+            dontPatch = true;
+            dontBuild = true;
+            dontStrip = true;
+            dontConfigure = true;
+            installPhase = ''
+              mkdir -p $out/bin
+              mkdir -p $out/lib/9.8.1
+              cp bin/haskell-language-server-9.8.1 bin/haskell-language-server-wrapper $out/bin/
+              cp lib/9.8.1/*.dylib $out/lib/9.8.1/
+              for f in ${ghc98}/lib/aarch64-osx-ghc-9.8.1/*.dylib
+              do
+                ln -s $f $out/lib/9.8.1/
               done
             '';
           };
